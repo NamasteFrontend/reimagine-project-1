9d26b51426592b3f350b41e73de75f6c964ea9d4
Bandhan Sarkar <bandhansarkar63@gmail.com>
2024-05-26 11:34:35 +0530

    new

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let nav = document.querySelector("nav");
-let navIcon = document.querySelector(".nav-icon");
+let navIcon = document.querySelector(".menu-icon");
 
 let flag = 1;
 
@@ -30,17 +30,53 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   
-//   document.addEventListener("DOMContentLoaded", function() {
-//     var hamburger = document.querySelector(".hamburger");
-//     hamburger.addEventListener("click", function() {
-//       this.classList.toggle("is-active");
-//       if (this.classList.contains("is-active")) {
-//         gsap.to(this, {duration: 0.5, rotation: 45});
-//       } else {
-//         gsap.to(this, {duration: 0.5, rotation: 0});
-//       }
-//     });
-//   });
-  
 
 
+
+
+
+
+let navTopMiddle = document.querySelector(".nav-top-middle");
+// let navTopMiddle = document.querySelector('.top-middle-content')
+// let navTopMiddle = document.querySelector(".nav-top");
+
+navTopMiddle.addEventListener("mouseenter", function () {
+    let tl = gsap.timeline()
+
+    tl.to(".nav-top", {
+        height: "23vw",
+        duration: 0.3,
+        // display: "block"
+    })
+    tl.to(".top-middle-content h5", {
+        display: "block",
+        transform: "translateY(0%)",
+        stagger: {
+            amount: 0.6
+        }
+        
+    })
+    
+
+})
+
+nav.addEventListener("mouseleave", function () {
+    let tl = gsap.timeline()
+
+    tl.to(".top-middle-content h5", {
+        transform: "translateY(100%)",
+        stagger: {
+            amount: 0.2
+        }
+    })
+    tl.to(".top-middle-content h5", {
+        display: "none",
+        duration: 0.01
+    })
+    tl.to(".nav-top", {
+        height: '50%',
+        duration: 0.2,
+        // display:"none"
+    })
+
+})
