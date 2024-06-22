fd2ce5270ccb291878f491f78eeb89b217d97a5d
sujoy-das-2004 <dr.dassujoy1597@gmail.com>
2024-06-22 12:05:26 +0530

    new add

---
+++ script.js
@@ -0,0 +1,25 @@
+const cursor = document.querySelector('.custom-cursor');
+const section = document.querySelector('.section-2');
+
+
+
+section.addEventListener('mouseenter', () => {
+    cursor.style.opacity = '100'; // Ensure cursor is visible when entering
+    cursor.style.transition = '3s'; // Apply transition for opacity
+    section.addEventListener('mousemove', (e) => {
+        const mouseX = window.innerWidth - e.clientX; // Calculate inverted X position
+        const mouseY = window.innerHeight - e.clientY; // Calculate inverted Y position
+    
+        cursor.style.left = `${mouseX}px`;
+        cursor.style.top = `${mouseY}px`;
+        
+    });
+    
+});
+section.addEventListener('mouseleave', () => {
+
+    section.removeEventListener('mousemove', handleMouseMove);
+});
+
+
+
