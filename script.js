3eed4d1126a5aa147d4636245fb82972216f8530
sujoy-das-2004 <dr.dassujoy1597@gmail.com>
2024-06-29 09:45:51 +0530

    first commmit

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const cursor = document.querySelector('.section-2-container');
 const section = document.querySelector('.section-2');
 
-section.addEventListener('mouseenter', () => {
-    gsap.to(cursor, { opacity: 1, duration: 3 });
+let isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-    section.addEventListener('mousemove', handleMouseMove);
-});
+if (isTouchDevice) {
+    section.addEventListener('touchstart', () => {
+        gsap.to(cursor, { opacity: 1, duration: 3 });
 
-section.addEventListener('mouseleave', () => {
-    section.removeEventListener('mousemove', handleMouseMove);
-});
+        section.addEventListener('touchmove', handleTouchMove);
+    });
+
+    section.addEventListener('touchend', () => {
+        section.removeEventListener('touchmove', handleTouchMove);
+    });
+} else {
+    section.addEventListener('mouseenter', () => {
+        gsap.to(cursor, { opacity: 1, duration: 3 });
+
+        section.addEventListener('mousemove', handleMouseMove);
+    });
+
+    section.addEventListener('mouseleave', () => {
+        section.removeEventListener('mousemove', handleMouseMove);
+    });
+}
 
 function handleMouseMove(e) {
     const mouseX = window.innerWidth - e.clientX;
@@ -22,15 +36,26 @@ function handleMouseMove(e) {
     });
 }
 
-document.addEventListener("mousemove", function(dets){
-    document.querySelectorAll(".img").forEach((elem) => {
-       const position = elem.getAttribute("value");
-       let x = (window.innerWidth - dets.clientX * position)/50;
-       let y = (window.innerHeight - dets.clientY * position)/50;
+function handleTouchMove(e) {
+    const touchX = window.innerWidth - e.touches[0].clientX;
+    const touchY = window.innerHeight - e.touches[0].clientY;
+    gsap.to(cursor, { 
+        left: touchX, 
+        top: touchY, 
+        duration: 2.1, 
+        ease: 'power1.out' 
+    });
+}
 
-      elem.style.transform = `translateX(${x}px) translateY(${y}px)`
-       gsap.to(elem,{
-        transform: `translateX(${x}px) translateY(${y}px)`
-       })
-    })
-})
+document.addEventListener(isTouchDevice ? "touchmove" : "mousemove", function(dets) {
+    document.querySelectorAll(".img").forEach((elem) => {
+        const position = elem.getAttribute("value");
+        let x = (window.innerWidth - (isTouchDevice ? dets.touches[0].clientX : dets.clientX) * position) / 50;
+        let y = (window.innerHeight - (isTouchDevice ? dets.touches[0].clientY : dets.clientY) * position) / 50;
+
+        elem.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        gsap.to(elem, {
+            transform: `translateX(${x}px) translateY(${y}px)`
+        });
+    });
+});
